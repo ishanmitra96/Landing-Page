c256258621684affc1c89d21012013f1b86a3938
ishanmitra <ishanmitra72@gmail.com>
2024-07-23 21:32:41 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navbar from './componantes/Navbar'
+import Hero from './componantes/Hero'
+
+function App() {
+  
+
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    </>
+  )
+}
+
+export default App
